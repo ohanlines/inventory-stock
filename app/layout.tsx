5fb0ce459e718cd345b090ca0a689e1c0c2d010d
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-02-08 19:22:26 +0700

    insert footer, catch error

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,17 @@ export default function RootLayout({
       */}
       <head />
       <body>{children}</body>
+
+            {/*
+      <footer>
+        <div className="text-center text-xs ">
+          <p>
+            ini footer paling keren
+              Dibuat @ rumah pak Djamalis kamar paling pojok
+          </p>
+        </div>
+      </footer>
+               */}
     </html>
   )
 }
