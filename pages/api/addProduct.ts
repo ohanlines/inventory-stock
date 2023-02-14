bc44a8dd238221efa0a2d36d22ae9992a0ab012f
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-02-14 17:26:30 +0700

    remove unnecessary code

--- pages/api/addProduct.ts
+++ pages/api/addProduct.ts
@@ -5,17 +5,17 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+    req: NextApiRequest,
+    res: NextApiResponse
 ) {
     if (req.method !== 'POST') {
         return res.status(405).json({message: 'Method Not Allowed'});
- }
- try {
-     const product = JSON.parse(req.body);
-     const saveProduct = await prisma.stock.create({data: product});
-     res.status(200).json(saveProduct);
- }   catch(err) {
-     res.status(400).json({message: 'Something Went Wrong Ohan'});
- }
+    }
+    try {
+        const product = JSON.parse(req.body);
+        const saveProduct = await prisma.stock.create({data: product});
+        res.status(200).json(saveProduct);
+    }   catch(err) {
+        res.status(400).json({message: 'Something Went Wrong Ohan'});
+    }
 };
--- pages/api/getProduct.ts
+++ pages/api/getProduct.ts
@@ -15,14 +15,3 @@ export default async function handler(
     return res.status(500).json(error)
   }
 }
-
-// type Data = {
-//   name: string
-// }
-
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
