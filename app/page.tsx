6be15edfcc8e1115a3221e8d37f0c8ea9a8d9e4a
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-05 19:19:19 +0700

    try headlessui combobox

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import AddProductForm from '../components/addProductForm'
-import GetProductsTable from '../components/productsTable'
+'use client'
 import Link from 'next/link'
+import Example from '../components/inputList'
 
 export default function Home() {
   return (
+    <>
     <Link href="/products">To Products</Link>
+    <Example />
+    </>
   );
 }
