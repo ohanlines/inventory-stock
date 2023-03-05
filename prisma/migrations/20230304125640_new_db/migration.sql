c8de2f07970a26a735348f1582f8ae17bf7b1730
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-05 19:18:19 +0700

    new db schema

--- prisma/migrations/20230304125640_new_db/migration.sql
+++ prisma/migrations/20230304125640_new_db/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE `Stock` (
     `product` VARCHAR(191) NOT NULL,
     `category` VARCHAR(191) NOT NULL,
     `price` INTEGER NOT NULL,
+    `stock` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
