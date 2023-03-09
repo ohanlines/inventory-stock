d175cae3d002c0b23051d216a7187758bd0b049d
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-09 08:38:36 +0700

    form submit functionallity

--- app/purchasing/page.tsx
+++ app/purchasing/page.tsx
@@ -1,8 +1,14 @@
 'use client'
 
 import useSWR, { preload } from 'swr'
-import { useForm } from 'react-hook-form'
-import Example from '../../components/inputList'
+import { useForm, useFieldArray } from 'react-hook-form'
+import InputList from '../../components/inputList'
+
+const styles = {
+  inputButton: "transision ease-in-out delay-10 border border-green-400 hover:bg-green-400 hover:scale-110 duration-10 px-2 mr-2 rounded",
+  numInputDiv: "cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm",
+  numInput: "border-none focus:outline-none py-2 pl-3 pr-3 text-sm leading-5 text-gray-900 focus:ring-0 w-16"
+}
 
 const fetcher = async () => {
   const res = await fetch(`/api/getProduct`)
@@ -10,17 +16,35 @@ const fetcher = async () => {
   return data
 }
 
-// const fetcher = (url: string) => axios.get(url).then(res => res.data)
-
 preload(`/api/getProduct`, fetcher)
 
 
 export default function Home() {
     const { data, isValidating } = useSWR('/api/getProduct', fetcher)
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, reset, control } = useForm({
+      defaultValues: {
+        cart: [{
+          product: '',
+          amount: ''
+        }]
+      }
+    })
     // const product = register('product', {required: true})
     // console.log("VALIDATING?: ", isValidating)
     // console.log("DATA SWR: ", data)
+
+    const { fields, append, remove } = useFieldArray({
+      name: 'cart',
+      control
+    })
+
+    const sumbitData = (data: any) => {
+      try {
+        console.log("DATA: ", data);
+      } catch (err) {
+        console.log("ERROR: ", err)
+      }
+    }
 
     // const coba = watch('product')
     // console.log("COBA: ", coba)
@@ -33,15 +57,54 @@ export default function Home() {
             <p>koko</p>
           </div>
 
-
             */}
-          <div className="flex justify-center">
+
+          <div className="flex bg-blue-400 justify-center">
             <form onSubmit={handleSubmit((data) => console.log("DATA: ", data))}>
-              <Example
-                name='coba'
-                register={register}
-                people={data}/>
-              <button type='submit'> Submit </button>
+              {fields.map((field, index) => {
+              return <section key={field.id} >
+                <div className="flex flex-row">
+                  <InputList
+                    name={`cart.${index}.product`}
+                    register={register}
+                    data={data}/>
+                </div>
+
+                <div className={styles.numInputDiv}>
+                  <input {...register(`cart.${index}.amount`, {required: true})}
+                         type='number'
+                         className={styles.numInput}/>
+                </div>
+
+                <button
+                  onClick={() => remove(index)}
+                  className={styles.inputButton}>
+                  Delete
+                </button>
+              </section>
+              })}
+
+
+              <div className="flex flex-row justify-end">
+                <button
+                  type='button'
+                  onClick={() => {
+
+                  append({
+                  product: '',
+                  amount: ''
+                  })
+                  }}
+                  className={styles.inputButton}>
+                  Add More..
+                </button>
+
+                <button
+                  className={styles.inputButton}
+                  type='submit'>
+                  Submit
+                </button>
+              </div>
             </form>
           </div>
         </>
--- components/inputList.tsx
+++ components/inputList.tsx
@@ -4,19 +4,19 @@ import { Fragment, useState } from 'react'
 import { Combobox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 
-export default function Example({ register, name, people }: any) {
-  const [selected, setSelected] = useState(people[0])
+export default function InputList({ register, name, data }: any) {
+  const [selected, setSelected] = useState(data[0])
   const [query, setQuery] = useState('')
 
   const hookForm = register(name)
   // const data = await products()
   // console.log("DATA TO SHOW: ", data)
 
-  const filteredPeople =
+  const filteredData =
     query === ''
-      ? people
-      : people.filter((person) =>
-          person.product
+      ? data
+      : data.filter((data) =>
+          data.product
             .toLowerCase()
             .replace(/\s+/g, '')
             .includes(query.toLowerCase().replace(/\s+/g, ''))
@@ -24,13 +24,14 @@ export default function Example({ register, name, people }: any) {
 
   return (
     <div className="">
-      <Combobox value={selected} onChange={setSelected}>
-        <div className="relative mt-1">
+      <Combobox  onChange={setSelected}>
+        <div className="relative">
           <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
             <Combobox.Input
               className="w-full border-none focus:outline-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-              displayValue={(person) => person.product}
+              displayValue={(data) => data.product}
               onChange={(event) => setQuery(event.target.value)}
+              placeholder="Product Name"
               {...register(name)}
             />
             <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
@@ -48,20 +49,20 @@ export default function Example({ register, name, people }: any) {
             afterLeave={() => setQuery('')}
           >
             <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-              {filteredPeople.length === 0 && query !== '' ? (
+              {filteredData.length === 0 && query !== '' ? (
                 <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                   Nothing found.
                 </div>
               ) : (
-                filteredPeople.map((person) => (
+                filteredData.map((data) => (
                   <Combobox.Option
-                    key={person.id}
+                    key={data.id}
                     className={({ active }) =>
                       `relative cursor-default select-none py-2 pl-10 pr-4 ${
                         active ? 'bg-teal-600 text-white' : 'text-gray-900'
                       }`
                     }
-                    value={person}
+                    value={data}
                   >
                     {({ selected, active }) => (
                       <>
@@ -70,7 +71,7 @@ export default function Example({ register, name, people }: any) {
                             selected ? 'font-medium' : 'font-normal'
                           }`}
                         >
-                          {person.product}
+                          {data.product}
                         </span>
                         {selected ? (
                           <span
