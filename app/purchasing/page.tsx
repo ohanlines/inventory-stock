1478c6c35c4d4f8479b4c5399ebf78afb6dcfb46
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-11 19:48:21 +0700

    showing error while add non-unique product

--- app/purchasing/page.tsx
+++ app/purchasing/page.tsx
@@ -8,7 +8,7 @@ import LoadingScreen from '../../components/loadingScreen'
 
 const styles = {
   inputButton: "transision ease-in-out delay-10 border border-green-400 hover:bg-green-400 hover:scale-110 duration-10 px-2 mr-2 rounded",
-  numInputDiv: "cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm mr-2",
+  numInputDiv: "cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm mr-2 ",
   numInput: "border-none focus:outline-none py-2 pl-3 pr-3 text-sm leading-5 text-gray-900 focus:ring-0 w-16"
 }
 
@@ -22,7 +22,7 @@ preload(`/api/getProduct`, fetcher)
 
 
 export default function Home() {
-    const [loading, setLoading] = useState(false)
+    const [showError, setShowError] = useState(false)
     const { data, isValidating, isLoading } = useSWR('/api/getProduct', fetcher)
     const { register, handleSubmit, reset, control } = useForm({
       defaultValues: {
@@ -55,8 +55,16 @@ export default function Home() {
 
     const submitData = (data: any) => {
       try {
-        reset()
-        console.log("DATA: ", data);
+        const dataArray = data.cart
+        const isProductUnique = new Set(dataArray.map(item => item.product)).size === dataArray.length
+
+        if (isProductUnique) {
+          console.log("DATA: ", data);
+          reset()
+        } else {
+          setShowError(true)
+        }
+
       } catch (err) {
         console.log("ERROR: ", err)
       }
@@ -70,7 +78,24 @@ export default function Home() {
             <p>koko</p>
           </div>
 
+            <div className="absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50">
             */}
+            <div className={`absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50 transition-all ${showError ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}>
+              <div className="">
+                <div className="bg-red-500 text-white p-8 rounded-lg">
+                  <div className="pb-2">
+                    Nama Product yang diinput Tidak Boleh Sama
+                  </div>
+                  <div className="flex justify-end pt-2">
+                    <div
+                      onClick={() => setShowError(false)}
+                      className="flex w-1/3 text-black bg-white rounded justify-center items-center cursor-pointer">
+                      Oke
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
           <div className="flex justify-center">
             <form>
