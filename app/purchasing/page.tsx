59805d32a567fabdceaf7fcb2560df1d8cd43229
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-12 02:40:31 +0700

    prevent input value outside of the db

--- app/purchasing/page.tsx
+++ app/purchasing/page.tsx
@@ -9,7 +9,7 @@ import LoadingScreen from '../../components/loadingScreen'
 const styles = {
   inputButton: "transision ease-in-out delay-10 border border-green-400 hover:bg-green-400 hover:scale-110 duration-10 px-2 mr-2 rounded",
   numInputDiv: "cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm mr-2 ",
-  numInput: "border-none focus:outline-none py-2 pl-3 pr-3 text-sm leading-5 text-gray-900 focus:ring-0 w-16"
+  numInput: "border-none focus:outline-none py-2 pl-3 pr-3 text-sm leading-5 text-gray-900 focus:ring-0 w-16",
 }
 
 const fetcher = async () => {
@@ -22,8 +22,9 @@ preload(`/api/getProduct`, fetcher)
 
 
 export default function Home() {
-    const [showError, setShowError] = useState(false)
-    const { data, isValidating, isLoading } = useSWR('/api/getProduct', fetcher)
+    const [showError, setShowError] = useState({ isActive: false, text: '' })
+    const [dataProductSet, setDataProductSet] = useState<Set<string>>(new Set());
+    const { data, error, isValidating, isLoading } = useSWR('/api/getProduct', fetcher)
     const { register, handleSubmit, reset, control } = useForm({
       defaultValues: {
         cart: [{
@@ -32,6 +33,7 @@ export default function Home() {
         }]
       }
     })
+
     // const product = register('product', {required: true})
     // console.log("VALIDATING?: ", isValidating)
     // console.log("DATA SWR: ", data)
@@ -40,6 +42,17 @@ export default function Home() {
       name: 'cart',
       control
     })
+
+    // Convert fetched data into set
+    useEffect(() => {
+      if (data && !error) {
+        const newSet = new Set(data.map((item: any) => item.product))
+        setDataProductSet(newSet)
+      }
+    }, [data, error])
+
+    // console.log("DATA SET", dataProductSet)
+    // console.log("DATA", data)
 
     // if (isLoading) return <LoadingScreen />
 
@@ -56,13 +69,35 @@ export default function Home() {
     const submitData = (data: any) => {
       try {
         const dataArray = data.cart
-        const isProductUnique = new Set(dataArray.map(item => item.product)).size === dataArray.length
+        const dataSet = new Set(dataArray.map((item: any) => item.product))
 
-        if (isProductUnique) {
-          console.log("DATA: ", data);
+        // check is user input unique product
+        const isProductUnique = dataSet.size === dataArray.length
+
+        // check is input exist inside db
+        const isProductExist = [...dataSet].every(value => dataProductSet.has(value))
+
+        // if statement for isProductUnique
+        // if (isProductUnique) {
+        //   console.log("DATA: ", data)
+        //   reset()
+        // } else {
+        //   setShowError(true)
+        // }
+
+        // BELOM SELESAI
+        // if statement for isProductDifferent
+
+        if (isProductExist) {
+          if (isProductUnique) {
+            console.log("DATA: ", data)
+          } else {
+            setShowError({isActive: true, text: 'Nama Product yang Diinput Tidak Boleh Sama'})
+            return
+          }
           reset()
         } else {
-          setShowError(true)
+          setShowError({isActive: true, text: 'Terjadi Kesalahan Input'})
         }
 
       } catch (err) {
@@ -79,16 +114,24 @@ export default function Home() {
           </div>
 
             <div className="absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50">
+            <ErrorFloating
+              comment="Nama Product yang diinput Tidak Boleh Sama"
+              error={showError}
+              setShowError={setShowError()} />
             */}
-            <div className={`absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50 transition-all ${showError ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}>
+
+            {/*
+              */}
+
+            <div className={`absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50 transition-all ${showError.isActive ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}>
               <div className="">
                 <div className="bg-red-500 text-white p-8 rounded-lg">
                   <div className="pb-2">
-                    Nama Product yang diinput Tidak Boleh Sama
+                    {showError.text}
                   </div>
                   <div className="flex justify-end pt-2">
                     <div
-                      onClick={() => setShowError(false)}
+                      onClick={() => setShowError({isActive: false, text: ''})}
                       className="flex w-1/3 text-black bg-white rounded justify-center items-center cursor-pointer">
                       Oke
                     </div>
