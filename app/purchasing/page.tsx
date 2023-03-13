1466eb403834427db9d838546787728c77f44c55
Ahmad Rauhan <rauhanhouse7@gmail.com>
2023-03-13 22:19:15 +0700

    displaying current stock beside num input

--- app/purchasing/page.tsx
+++ app/purchasing/page.tsx
@@ -8,8 +8,8 @@ import LoadingScreen from '../../components/loadingScreen'
 
 const styles = {
   inputButton: "transision ease-in-out delay-10 border border-green-400 hover:bg-green-400 hover:scale-110 duration-10 px-2 mr-2 rounded",
-  numInputDiv: "cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm mr-2 ",
-  numInput: "border-none focus:outline-none py-2 pl-3 pr-3 text-sm leading-5 text-gray-900 focus:ring-0 w-16",
+  numInputDiv: "relative cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm mr-2 ",
+  numInput: "border-none focus:outline-none py-2 pl-3 pr-8 text-sm leading-5 text-gray-900 focus:ring-0 w-20",
 }
 
 const fetcher = async () => {
@@ -18,14 +18,11 @@ const fetcher = async () => {
   return data
 }
 
-preload(`/api/getProduct`, fetcher)
-
-
 export default function Home() {
     const [showError, setShowError] = useState({ isActive: false, text: '' })
     const [dataProductSet, setDataProductSet] = useState<Set<string>>(new Set());
     const { data, error, isValidating, isLoading } = useSWR('/api/getProduct', fetcher)
-    const { register, handleSubmit, reset, control } = useForm({
+    const { register, handleSubmit, reset, control, getValues, watch } = useForm({
       defaultValues: {
         cart: [{
           product: '',
@@ -33,23 +30,21 @@ export default function Home() {
         }]
       }
     })
-
-    // const product = register('product', {required: true})
-    // console.log("VALIDATING?: ", isValidating)
-    // console.log("DATA SWR: ", data)
-
     const { fields, append, remove } = useFieldArray({
       name: 'cart',
       control
     })
 
-    // Convert fetched data into set
+    // === CONVER FETCHED DATA INTO SET ===================
     useEffect(() => {
       if (data && !error) {
         const newSet = new Set(data.map((item: any) => item.product))
         setDataProductSet(newSet)
       }
     }, [data, error])
+
+    // === LOADING SCREEN WITH TIMEOUT =====================
+    if (isValidating) return <LoadingScreen />
 
     // console.log("DATA SET", dataProductSet)
     // console.log("DATA", data)
@@ -64,8 +59,19 @@ export default function Home() {
     //   }, 1000)
     // }
 
-    if (isValidating) return <LoadingScreen />
+    // === GET STOCK FOR NUMBER INPUT DISPLAY ==============
+    const getStockByProduct = (product: String) => {
+      const dataProduct = data?.find((item: any) =>item.product === product)
+      const result = dataProduct?.stock
+      return result
+    }
 
+    // === ONCHANGE USING WATCH ============================
+    const value = watch("cart")
+    const valueMap = value.map((data) => data.product)
+
+
+    // === HANDLING SUMBIT FORM ============================
     const submitData = (data: any) => {
       try {
         const dataArray = data.cart
@@ -76,17 +82,6 @@ export default function Home() {
 
         // check is input exist inside db
         const isProductExist = [...dataSet].every(value => dataProductSet.has(value))
-
-        // if statement for isProductUnique
-        // if (isProductUnique) {
-        //   console.log("DATA: ", data)
-        //   reset()
-        // } else {
-        //   setShowError(true)
-        // }
-
-        // BELOM SELESAI
-        // if statement for isProductDifferent
 
         if (isProductExist) {
           if (isProductUnique) {
@@ -107,22 +102,6 @@ export default function Home() {
 
     return (
         <>
-          {/*
-          <div>
-            <p>coba</p>
-            <p>koko</p>
-          </div>
-
-            <div className="absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50">
-            <ErrorFloating
-              comment="Nama Product yang diinput Tidak Boleh Sama"
-              error={showError}
-              setShowError={setShowError()} />
-            */}
-
-            {/*
-              */}
-
             <div className={`absolute inset-0 flex justify-center items-center backdrop-filter backdrop-blur-sm z-50 transition-all ${showError.isActive ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}>
               <div className="">
                 <div className="bg-red-500 text-white p-8 rounded-lg">
@@ -155,6 +134,9 @@ export default function Home() {
                   <input {...register(`cart.${index}.amount`, {required: true})}
                          type='number'
                          className={styles.numInput}/>
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+                    <p className="text-gray-300">/ {getStockByProduct(valueMap[index])}</p>
+                  </div>
                 </div>
 
                 <button
